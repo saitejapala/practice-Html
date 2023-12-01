a7431edd6acd04ca2d58929f757bd6f418f0fc66
Sai Teja <saitejapala+github@gmail.com>
2023-12-01 18:18:58 +0530

    js file is created and linked to the html file and some necessary changes made in the grading.html

--- JS files/grading.js
+++ JS files/grading.js
@@ -1,6 +1,26 @@
-function getResults(){
+function getResult(){
     let hm,cm,jm,tot,avg,grd;
-    hm=+css.value;
+    hm=+html.value;
+    cm=+css.value;
     jm=+js.value;
     bm=+bs.value;
+
+    // claculations
+    tot=hm+cm+jm+bm;
+    avg=tot/4;
+
+    //Checking the grade
+    if(hm>39 && cm>39 && jm>39 && bm>39){
+        //pass
+        grd=avg>89? "A" : avg>79? "B" : avg>69? "B+" : avg>59? "B" : avg>49? "C" : "D"
+    }
+    else{
+        grd="Fail";
+    }
+
+    //Displaying
+    total.value="Total Marks : " +tot;
+    average.value="Average : " +avg;
+    grade.value="Grade : " +grd;
+
 }
